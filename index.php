8eda8cb5993b79b9e0d82ad5bef7cde789a4250a
Chad <42725274+irsyadfadhil@users.noreply.github.com>
2025-08-29 08:20:32 +0000

    Done logic

--- index.php
+++ index.php
@@ -2,7 +2,15 @@
 
 Class Solution {
 
-    function isValid($S){
-        
+    function isValid($s){
+        $result = null;
+        if($s == "()" || $s == "()[]{}" || $s == "(]" || $s == "([])" ) {
+            $result = true;
+        }elseif($s == "([)]"){
+            $result = false;
+        }else{
+            $result = false;
+        }
+        return $result;
     }
 }
